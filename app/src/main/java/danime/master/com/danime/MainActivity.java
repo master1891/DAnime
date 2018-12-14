986b5e5c95176b1d013b23eaa1c3584a423675c6
Nelson Ek <neltip@hotmail.com>
2018-12-14 09:39:38 -0600

    fuentes y bottom toolbar

--- app/src/main/java/danime/master/com/danime/MainActivity.java
+++ app/src/main/java/danime/master/com/danime/MainActivity.java
@@ -3,19 +3,19 @@ package danime.master.com.danime;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
 
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
-import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.bottom_navigation)
-    AHBottomNavigation ahBottomNavigation;
+    BottomNavigation bottomNavigation;
 
 
     @Override
@@ -32,33 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void configItemsBottomNavigation(){
-
-
-        ahBottomNavigation.setDefaultBackgroundColor(getResources().getColor(R.color.colorPrimary));
-
-
-        AHBottomNavigationItem ahBottomHome = new AHBottomNavigationItem(R.string.home,R.drawable.ic_home_outline,R.color.colorAccent);
-        AHBottomNavigationItem ahBottomFavorites= new AHBottomNavigationItem(R.string.downloading,R.drawable.ic_favorite_outline,R.color.colorAccent);
-        AHBottomNavigationItem ahBottomVideos= new AHBottomNavigationItem(R.string.completes,R.drawable.ic_videos,R.color.colorAccent);
-        ahBottomNavigation.setForceTint(true);
-        ahBottomNavigation.setAccentColor(R.color.colorAccent);
-        //agregando los items al toolbar
-        this.ahBottomNavigation.addItem(ahBottomHome);
-        this.ahBottomNavigation.addItem(ahBottomFavorites);
-        this.ahBottomNavigation.addItem(ahBottomVideos);
-
+        
     }
 
 
     private void initEvents(){
-        ahBottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
-            @Override
-            public boolean onTabSelected(int position, boolean wasSelected) {
-                return false;
-            }
-        });
+
     }
-
-
 
 }
---
+++ app/src/main/java/danime/master/com/danime/Util/UtilFont.java
@@ -0,0 +1,12 @@
+package danime.master.com.danime.Util;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class UtilFont {
+
+    public static void ApplyRaleway(Context context, TextView view){
+
+    }
+
+}
